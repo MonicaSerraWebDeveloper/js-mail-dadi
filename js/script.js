695d62d902fb8b9e5e30696cb35dca1e3aa9e409
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-08 16:04:54 +0100

    feat: add all the sequence of the ex dice created the random number for the player and the computer

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+// Impostare due giocatori: giocatore vs computer
+let playerDice = Math.floor((Math.random() * 6) + 1);
+console.log(playerDice);
+let computerDice = Math.floor((Math.random() * 6) + 1);
+console.log(computerDice);
 
+
+// Far uscire un numero random da 1 a 6 per il giocatore
+// Far uscire un numero random da 1 a 6 per il computer
+// Se il giocatore ha un numero maggiore del computer allora vince
+// Se il computer ha un numero maggiore del giocatore allora vince
+// Se il giocatore e il computer hanno stesso numero è patta
 
 
 
